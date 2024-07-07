fe6903a689424264588b02f132b5fff8890c604a
Morgan Hondros <morganhondros@gmail.com>
2024-07-07 06:05:08 -0400

    Blackjack game added.

--- Python/playGround.py
+++ Python/playGround.py
@@ -1,9 +1,7 @@
-
-fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
-vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+def outer_function(a, b):
+    def inner_function(c, d):
+        return c + d
+    return inner_function(a, b)
  
-dirty_dozen = [fruits, vegetables]
- 
-print(dirty_dozen[1][1])
-
-print(dirty_dozen)
+result = outer_function(5, 10)
+print(result)
